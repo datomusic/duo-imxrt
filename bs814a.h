eeed7602fec056903cce4ce84e0d6b9d65db06d7
David Menting <david@davidmenting.nl>
2022-07-26 21:11:05 +0200

    BS814A-2: only read data when touch is detected

--- bs814a.h
+++ bs814a.h
@@ -3,6 +3,7 @@
 #include "fsl_iomuxc.h"
 #include "fsl_gpio.h"
 
+// TODO: Switch to Arduino pin mappings once those are available
 #define BS814A_CLOCK_PERIOD_US 20
 #define BS814A_CLOCK_GPIO GPIO1
 #define BS814A_CLOCK_PIN 3U
@@ -14,7 +15,8 @@
 void BS814A_begin(void);
 uint8_t BS814A_readRaw(void);
 
-void BS814A_begin() {
+void BS814A_begin()
+{
   // Clock pin configuration
   IOMUXC_SetPinMux(BS814A_CLOCK_MUX, 0U);
   IOMUXC_SetPinConfig(BS814A_CLOCK_MUX , 0x10B0U);
@@ -29,26 +31,34 @@ void BS814A_begin() {
   GPIO_PinInit(BS814A_DATA_GPIO, BS814A_DATA_PIN, &input_config);
 }
 
-/* Bit 0:3 are Key 1 to Key 4 state. 0 is touched, 1 is no touch 
-   * Bit 4:6 is a checksum. The number of keys touched
-   * Bit 7 is a stop bit. It is always 1
-   
-struct {
-  
-} BS814_Response;
+/* 
+  Bit 0:3 are Key 1 to Key 4 state. 0 is touched, 1 is no touch 
+  Bit 4:6 is a checksum. The number of keys touched
+  Bit 7 is a stop bit. It is always 1
+
+  The BS814A pulls the data pin low to signal that a touch is detected
 */
-uint8_t BS814A_readRaw() {
-  // Read an 8 bit data byte from BS814A
+uint8_t BS814A_readRaw() 
+{
+  if (GPIO_PinRead(BS814A_DATA_GPIO, BS814A_DATA_PIN) == 1)
+  {
+    return 0;
+  }
+
+  uint8_t value = 0;
   uint8_t mask;
-  uint8_t value=0;
-  for (mask=0x01; mask; mask <<= 1) { // LSBFIRST
+  for (mask=0x01; mask; mask <<= 1) // LSBFIRST
+  { 
     // digitalWrite(BS814A_CLOCK_PIN, LOW);
     GPIO_PinWrite(BS814A_CLOCK_GPIO, BS814A_CLOCK_PIN, 0);
 
     delayMicroseconds(BS814A_CLOCK_PERIOD_US);
+
     if (GPIO_PinRead(BS814A_DATA_GPIO, BS814A_DATA_PIN)) value |= mask;
+
     // digitalWrite(BS814A_CLOCK_PIN, HIGH);
     GPIO_PinWrite(BS814A_CLOCK_GPIO, BS814A_CLOCK_PIN, 1);
+
     delayMicroseconds(BS814A_CLOCK_PERIOD_US);
   }
   return value; 
