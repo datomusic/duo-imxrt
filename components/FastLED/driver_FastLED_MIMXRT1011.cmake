153f2b3f91b0ad9e74dccfe743ad5afd05319900
Sundström Valter <mail@valter.nu>
2022-07-28 23:57:52 +0200

    Make FastLED build by setting appropriate defines

--- components/FastLED/driver_FastLED_MIMXRT1011.cmake
+++ components/FastLED/driver_FastLED_MIMXRT1011.cmake
@@ -9,4 +9,10 @@ target_include_directories(${MCUX_SDK_PROJECT_NAME} PRIVATE
     ${CMAKE_CURRENT_LIST_DIR}/.
 )
 
+# FastLED does not include Arduino.h without this define.
+add_definitions(-DARDUINO)
+
+# Silence warning about C++17 ignoring the `register` keyword.
+set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -Wno-register")
+
 include(driver_common_MIMXRT1011)
