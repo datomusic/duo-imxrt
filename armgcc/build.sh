ca8b7abe2c0d979dc80873c8041b73cf64c40bad
Sundström Valter <mail@valter.nu>
2022-07-30 22:51:40 +0200

    Allow toolchain script to work when TOOLCHAIN_DIR is not set

--- armgcc/build.sh
+++ armgcc/build.sh
@@ -6,7 +6,7 @@ if [[ -z "$BUILD_TYPE" ]]; then
   BUILD_TYPE=debug
 fi
 
-TOOLCHAIN_DIR?="/usr"
+TOOLCHAIN_DIR="${TOOLCHAIN_DIR:/usr}"
 
 cmake -DTOOLCHAIN_DIR="$TOOLCHAIN_DIR" -DCMAKE_TOOLCHAIN_FILE="./arm-none-eabi-gcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE  .
 make -j 2>&1 | tee build_log.txt
