637bb2c2c0408d85f793ac897e7c7f2dd1e55227
Sundström Valter <mail@valter.nu>
2022-08-23 18:24:08 +0200

    Restore arduino-rt1011.cpp

--- src/main/arduino-rt1011.cpp
+++ src/main/arduino-rt1011.cpp
@@ -1,9 +1,8 @@
-#include "Arduino.h"
 #include "pin_mux.h"
-/* #define TEENSYDUINO 999 */
+#include "Arduino.h"
+#define TEENSYDUINO 999
 // #include "Audio.h"
 #include "bs814a.h"
-/* #include "./Adafruit_NeoPixel.h" */
 
 // GUItool: begin automatically generated code
 // AudioSynthWaveformSine   sine1;          //xy=305,275
@@ -17,65 +16,44 @@
 /*!
  * @brief Main function
  */
+int main(void)
+{
+    /* Board pin init */
+    BOARD_ConfigMPU();
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitDebugConsole();
+    BOARD_InitNeopixels();
+    init(); // Seeeduino init
 
-/* #define PIN        NEOPIXEL_PIN */
-/* #define NUMPIXELS 8 */
+    uint8_t blue[] = {0x00,0x00,0xff};
+    board_rgb_write(blue);
+    BS814A_begin();
 
-/* Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); */
-/* #define DELAYVAL 500 */
+    #define PIN_MQS_R D7
+    #define PIN_AMP_MUTE PIN_A2
+    #define PIN_HP_ENABLE 23U
+    pinMode(PIN_MQS_R, OUTPUT); // AD_02, MQS pin, pin 58
+    pinMode(PIN_AMP_MUTE, OUTPUT); // AD_10, /AMP_MUTE pin 47
+    pinMode(PIN_HP_ENABLE, OUTPUT); // AD_11, /HP_ENABLE pin 46
+    digitalWrite(PIN_AMP_MUTE, LOW);
+    digitalWrite(PIN_HP_ENABLE, HIGH);
+    
+    // AudioMemory(100);
+    // sine1.amplitude(0.5F);
+    // sine1.frequency(440.0F);
 
-/* void setup() { */
-/* #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000) */
-/*   clock_prescale_set(clock_div_1); */
-/* #endif */
-
-/* } */
-
-/* void loop() { */
-/*   pixels.clear(); */
-
-/*   for(int i=0; i<NUMPIXELS; i++) { */
-
-/*     pixels.setPixelColor(i, pixels.Color(0, 150, 0)); */
-/*     pixels.show(); */
-/*     delay(DELAYVAL); */
-/*   } */
-/* } */
-
-int main(void) {
-  /* Board pin init */
-  BOARD_ConfigMPU();
-  BOARD_InitBootPins();
-  BOARD_InitBootClocks();
-  BOARD_InitDebugConsole();
-  BOARD_InitNeopixels();
-  init(); // Seeeduino init
-
-  /* pixels.begin(); */
-
-  int counter = 0;
-  int counter2 = 0;
-
-  const int PIXEL_COUNT = 4;
-  Pixel pixels[PIXEL_COUNT] = {
-    Pixel{0, 0xFF, 0},
-    Pixel{0, 0, 0},
-    Pixel{0, 0xFF, 0},
-    Pixel{0xFF, 0x0, 0x0}
-  };
-
-  while (1) {
-    if (++counter > 1000) {
-      ++pixels[1].b;
-      counter = 0;
-      board_rgb_write(pixels, PIXEL_COUNT);
+    while (1)
+    {
+        uint8_t value = BS814A_readRaw();
+        uint8_t g = (1-bitRead(value, 1)) * 100;
+        uint8_t b = (1-bitRead(value, 2)) * 100;
+        uint8_t r = (1-bitRead(value, 3)) * 100;
+        uint8_t potRGB[] = {r,g,b};
+        digitalWrite(PIN_MQS_R, LOW);
+        delayMicroseconds(2000);
+        board_rgb_write(potRGB);
+        digitalWrite(PIN_MQS_R, HIGH);
+        delayMicroseconds(2020);
     }
-
-    if (++counter2 > 4000) {
-      ++pixels[2].r;
-      counter2 = 0;
-    }
-
-    delayMicroseconds(1);
-  }
 }
