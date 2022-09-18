86af622d30bc4af218e3cff2eb9c08f171c6ed6a
Sundström Valter <mail@valter.nu>
2022-09-18 22:54:40 +0200

    Add helper scripts to easier build demos

---
+++ demos/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set -e
+
+if [[ -e "$1" ]]; then
+  TOOLCHAIN_DIR="${TOOLCHAIN_DIR:/usr}"
+  
+  pushd ./"$1"
+  cmake -DTOOLCHAIN_DIR="$TOOLCHAIN_DIR" -DCMAKE_TOOLCHAIN_FILE="../../armgcc/arm-none-eabi-gcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=flexspi_nor_debug  -S . -B build
+  pushd build
+  make -j
+  popd
+else
+  echo "Error: Supply directory containing demo to build"
+fi
