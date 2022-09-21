53f84396f0bb69aa451cb6465c9dc8df539d1c9e
David Menting <david@davidmenting.nl>
2022-09-21 21:39:53 +0200

    Added CMakeLists.txt for MQS demo

--- demos/build.sh
+++ demos/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ -e "$1" ]]; then
-  TOOLCHAIN_DIR="${TOOLCHAIN_DIR:/usr}"
+  TOOLCHAIN_DIR="${TOOLCHAIN_DIR}"
   
   pushd ./"$1"
   cmake -DTOOLCHAIN_DIR="$TOOLCHAIN_DIR" -DCMAKE_TOOLCHAIN_FILE="../../armgcc/arm-none-eabi-gcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=flexspi_nor_debug  -S . -B build
