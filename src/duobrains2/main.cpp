23b168f8b8f0fda401fd018df12265a5a4abfdfa
Sundström Valter <mail@valter.nu>
2022-10-10 00:14:49 +0200

    Move all globals copied from src.ini into globals.h

--- src/duobrains2/main.cpp
+++ src/duobrains2/main.cpp
@@ -1,23 +1,17 @@
 #include "Arduino.h"
 #include "pin_mux.h"
+#include "board_init.h"
 
 #include "stubs/sequencer_stub.h"
+#include "globals.h"
 #include "Sequencer.h"
 
 #include "stubs/leds_stub.h"
 #include "Leds.h"
 
-static void init_board(){
-  BOARD_ConfigMPU();
-  BOARD_InitBootPins();
-  BOARD_InitBootClocks();
-  BOARD_InitDebugConsole();
-  init(); // Seeeduino init
-}
 
 int main(void) {
-  init_board();
-
+  board_init();
 
   while (1) {
     delayMicroseconds(2020);
