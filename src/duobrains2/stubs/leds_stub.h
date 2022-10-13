c44aa69ccad98823667d72903930fa653732f174
Valter Sundström <valter.sundstrom@gmail.com>
2022-10-14 01:04:20 +0200

    Add Duo 2 pin mappings (#27)
    Co-authored-by: Sundström Valter <mail@valter.nu>

--- src/duobrains2/stubs/leds_stub.h
+++ src/duobrains2/stubs/leds_stub.h
@@ -1,20 +1,12 @@
-#ifndef LEDS_STUB_H_DY6XTB7P
-#define LEDS_STUB_H_DY6XTB7PLEDS_STUB_H_DY6XTB7P
+#ifndef LEDS_STUB_H_1WAP5UXR
+#define LEDS_STUB_H_1WAP5UXR
 
-const int ENV_LED = 0;
-const int FILTER_LED = 0;
-const int OSC_LED = 0;
-const int LED_CLK = 1;
 const int GRB = 1;
 const int SK6812 = 1;
-const int LED_DATA = 1;
-
-const int led_order[NUM_LEDS] = {1};
-
 
 struct Peak {
   float read() { return 0; };
 };
 Peak peak1;
 
-#endif /* end of include guard: LEDS_STUB_H_DY6XTB7PLEDS_STUB_H_DY6XTB7P */
+#endif /* end of include guard: LEDS_STUB_H_1WAP5UXR */
