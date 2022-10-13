bcdc104210ec13ad3d60f781e9d9652a6da99a2c
Valter Sundström <valter.sundstrom@gmail.com>
2022-10-14 00:13:17 +0200

    Minimal implementation for FastLED adapter (#25)
    * Add minimal implementation for FastLED adapter
    
    * Move FastLED color types to adapters/fast_led
    
    Co-authored-by: Sundström Valter <mail@valter.nu>

--- src/duobrains2/adapters/FastLED.h
+++
@@ -1,4 +0,0 @@
-#ifndef FASTLED_H_ACDOEHJU
-#define FASTLED_H_ACDOEHJU
-#include "../stubs/fastled_stub.h"
-#endif /* end of include guard: FASTLED_H_ACDOEHJU */
--- src/duobrains2/duo2/main.cpp
+++ src/duobrains2/duo2/main.cpp
@@ -1,21 +1,41 @@
 #include "Arduino.h"
-#include "pin_mux.h"
 #include "board_init.h"
+#include "pin_mux.h"
+#include "leds.h"
 
 #include "stubs/sequencer_stub.h"
 #include "globals.h"
 #include "duo1/Sequencer.h"
 
+const int NUM_LEDS = 19;
+
 #include "stubs/leds_stub.h"
 #include "duo1/Leds.h"
 
-
 int main(void) {
   board_init();
+  LEDs::init();
+
+  FastLED.addLeds<LED_TYPE, LED_DATA, COLOR_ORDER>(physical_leds, NUM_LEDS);
+
+  int counter = 0;
+for (int i = 0; i < NUM_LEDS; ++i) {
+    physical_leds[i] = 0;
+  }
+
 
   while (1) {
-    delayMicroseconds(2020);
+    if (counter >= NUM_LEDS) {
+      counter = 0;
+      for (int i = 0; i < NUM_LEDS; ++i) {
+        physical_leds[i] = 0;
+      }
+    } else {
+      physical_leds[counter].g = 140;
+      counter = counter + 1;
+    }
+
+    FastLED.show();
+    delayMicroseconds(400000);
   }
 }
-
-
