e60d2691e93816e67f7d6e793e5ea1f7cb71c7db
Sundström Valter <mail@valter.nu>
2022-10-14 16:21:46 +0200

    Add implementation for FastLED clear

--- src/duobrains2/adapters/fast_led/FastLED.h
+++ src/duobrains2/adapters/fast_led/FastLED.h
@@ -8,15 +8,22 @@ struct FastLED_ {
   void setBrightness(int);
   void setCorrection(int);
 
-  template <int A, int B, int C>
+  // We ignore the template parameters, since our current
+  // implementation is specific to a single chip+board currently.
+  // We also implement add_leds wrongly, since we replace all pixels
+  // rather than adding to them, which is also fine for current use-cases.
+  // The current goal is simply to implement the FastLED interface well enough
+  // for the Duo firmware to work.
+  // For future work it makes more sense to properly port FastLED and use that.
+  template <int _LedType, int _LedData, int _ColorOrder>
   void addLeds(CRGB *raw_pixels, const int count) {
-    pixel_count = count;
-    crgbs = raw_pixels;
+    this->pixel_count = count;
+    this->raw_pixels = raw_pixels;
   }
 
 private:
   int pixel_count;
-  CRGB *crgbs;
+  CRGB *raw_pixels;
 };
 
 FastLED_ FastLED;
