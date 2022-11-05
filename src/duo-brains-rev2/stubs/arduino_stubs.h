0080ba6a9a092b3b58bdc3e46d4a2b5e1a06278c
Sundström Valter <mail@valter.nu>
2022-11-06 00:37:08 +0100

    Add stubs

---
+++ src/duo-brains-rev2/stubs/arduino_stubs.h
@@ -0,0 +1,35 @@
+#ifndef ARDUINO_STUBS_H_SIADU43M
+#define ARDUINO_STUBS_H_SIADU43M
+
+long random(long _a, long _b){
+  // TODO
+  return 0;
+}
+
+long random(long _){
+  // TODO
+  return 0;
+}
+
+int touchRead(int8_t pin){
+  // TODO
+  return 0;
+}
+
+int eeprom_read_byte(int _){
+  // TODO
+  return 0;
+}
+
+
+int eeprom_write_byte(int _a, int _b){
+  // TODO
+  return 0;
+}
+
+long map(long, long, long, long, long){
+  return 0;
+}
+
+
+#endif /* end of include guard: ARDUINO_STUBS_H_SIADU43M */
---
+++ src/duo-brains-rev2/stubs/power_stubs.h
@@ -0,0 +1,7 @@
+#ifndef POWER_STUBS_H_DCKXBMDS
+#define POWER_STUBS_H_DCKXBMDS
+
+/* void power_off() { */
+/*   // TODO */
+/* } */
+#endif /* end of include guard: POWER_STUBS_H_DCKXBMDS */
