936adbdac58e07b0020ef98e3a06824d09094171
Sundström Valter <mail@valter.nu>
2022-11-06 12:52:54 +0100

    Add lib/audio

---
+++ src/lib/audio.h
@@ -0,0 +1,9 @@
+#ifndef AUDIO_H_OVLTWHKY
+#define AUDIO_H_OVLTWHKY
+
+namespace Audio {
+void headphone_enable(void);
+void amp_enable(void);
+} // namespace Audio
+
+#endif /* end of include guard: AUDIO_H_OVLTWHKY */
