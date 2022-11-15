672eac4ab5ae39619873f37beb6698339a275e6e
Sundström Valter <mail@valter.nu>
2022-11-15 17:09:26 +0100

    Rename boards and boards dir

--- src/duo-imxrt/duo/board/2_0-beta1/board_audio_output.h
+++
@@ -1,8 +0,0 @@
-#ifndef AUDIO_OUTPUT_H_VS0HQTKT
-#define AUDIO_OUTPUT_H_VS0HQTKT
-
-#include "output_mqs.h"
-
-typedef AudioOutputMQS BoardAudioOutput;
-
-#endif /* end of include guard: AUDIO_OUTPUT_H_VS0HQTKT */
