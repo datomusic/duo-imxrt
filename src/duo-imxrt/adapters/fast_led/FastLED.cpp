a18b7574dbfdbb3e50764cfefa8e7b88fbe38de5
Sundström Valter <mail@valter.nu>
2022-11-22 18:04:21 +0100

    Fix clear in FastLED adapter

--- src/duo-imxrt/adapters/fast_led/FastLED.cpp
+++ src/duo-imxrt/adapters/fast_led/FastLED.cpp
@@ -2,16 +2,16 @@
 #include "lib/leds.h"
 
 void FastLED_::show() {
-  LEDs::show(raw_pixels, pixel_count);
+   LEDs::show(raw_pixels, pixel_count);
 }
 
 void FastLED_::clear() {
-  memset(raw_pixels, 0, sizeof(raw_pixels));
+  memset(raw_pixels, 0, sizeof(CRGB) * pixel_count);
 }
 
-void FastLED_::setBrightness(int){
+void FastLED_::setBrightness(int) {
   // TODO
 }
-void FastLED_::setCorrection(int){
+void FastLED_::setCorrection(int) {
   // TODO
 }
