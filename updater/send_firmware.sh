b76657f54de6ef266a847e4a543b4215253bc568
Sundström Valter <mail@valter.nu>
2022-11-28 23:49:27 +0100

    Include flash config

--- updater/send_firmware.sh
+++ updater/send_firmware.sh
@@ -17,14 +17,14 @@ blhost -u 0x15A2,0x0073 -j -- get-property 1 0
 blhost -u 0x15A2,0x0073 -j -- fill-memory 0x20202000 4 0xC0000007 word
 blhost -u 0x15A2,0x0073 -j -- fill-memory 0x20202004 4 0 word
 blhost -u 0x15A2,0x0073 -j -- configure-memory 9 0x20202000
-blhost -u 0x15A2,0x0073 -j -- write-memory 0x60000400 ./fcb_w25q.bin,512
+# blhost -u 0x15A2,0x0073 -j -- write-memory 0x60000400 ./fcb_w25q.bin,512
 
 echo "erasing flash"
-blhost -u 0x15A2,0x0073 -- flash-erase-region 0x60001000 128000
+blhost -u 0x15A2,0x0073 -- flash-erase-region 0x60000400 128000
 echo "done"
 
 echo "writing binary"
-blhost -u 0x15A2,0x0073 -- write-memory 0x60001000 ../src/duo-imxrt/build/flexspi_nor_debug/out.bin
+blhost -u 0x15A2,0x0073 -- write-memory 0x60000400 ../src/demos/leds/build/flexspi_nor_debug/out.bin
 echo "done"
 
 blhost -u 0x15A2,0x0073 -- reset
