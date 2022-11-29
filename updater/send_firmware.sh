841860a0b44c6e5fdeb26b2a5964079e6f756562
Sundström Valter <mail@valter.nu>
2022-11-29 20:23:47 +0100

    update_firmware.py

--- updater/send_firmware.sh
+++ updater/send_firmware.sh
@@ -11,20 +11,21 @@ sdphost -u 0x1FC9,0x0145 -- jump-address 0x20205800
 
 # The MCU will now reconnect as a USB composite device
 sleep 1
-blhost -u 0x15A2,0x0073 -j -- get-property 1 0 
+blhost -u 0x15A2,0x0073  -- get-property 1 0
 
 ## Configure how the bootloader should address the flash memory
-blhost -u 0x15A2,0x0073 -j -- fill-memory 0x20202000 4 0xC0000007 word
-blhost -u 0x15A2,0x0073 -j -- fill-memory 0x20202004 4 0 word
-blhost -u 0x15A2,0x0073 -j -- configure-memory 9 0x20202000
-# blhost -u 0x15A2,0x0073 -j -- write-memory 0x60000400 ./fcb_w25q.bin,512
+blhost -u 0x15A2,0x0073  -- fill-memory 0x20202000 4 0xC0000007
+blhost -u 0x15A2,0x0073  -- fill-memory 0x20202004 4 0
+blhost -u 0x15A2,0x0073  -- configure-memory 9 0x20202000
+blhost -u 0x15A2,0x0073  -- write-memory 0x60000400 ./fcb_w25q.bin,512
 
-echo "erasing flash"
+echo "Erasing flash"
 blhost -u 0x15A2,0x0073 -- flash-erase-region 0x60000400 128000
+echo "Done"
+
+echo "Writing binary"
+blhost -u 0x15A2,0x0073 -- write-memory 0x60000400 ../src/duo-imxrt/build/flexspi_nor_release/out.bin
 echo "done"
 
-echo "writing binary"
-blhost -u 0x15A2,0x0073 -- write-memory 0x60000400 ../src/demos/leds/build/flexspi_nor_debug/out.bin
-echo "done"
-
+echo "Resetting"
 blhost -u 0x15A2,0x0073 -- reset
--- updater/update_firmware.py
+++ updater/update_firmware.py
@@ -1,85 +1,129 @@
-import rtmidi
-from rtmidi.midiutil import list_output_ports, open_midioutput
-from os.path import basename, exists, isdir, join
-import argparse
-import logging
-import os
 import sys
 import time
+import rtmidi
+from rtmidi.midiutil import open_midioutput
+from os.path import basename
+from spsdk.sdp import SDP
+import spsdk.sdp.interfaces.usb as sdp_usb
+import spsdk.mboot.interfaces.usb as mboot_usb
+from spsdk.mboot import McuBoot
 
-log = logging.getLogger("sendsysex")
 
-__program__ = 'sendsysex.py'
-__version__ = '1.1'
-__author__ = 'Christopher Arndt'
-__date__ = '$Date$'
-
-SYSTEM_EXCLUSIVE = b'\xF0'
-END_OF_EXCLUSIVE = b'\xF7'
-
-try:
-    raw_input
-except NameError:
-    # Python 3
-    raw_input = input
-
-def send_sysex_file(filename, midiout, portname, prompt=True, delay=50):
+def send_sysex_file(filename, midiout, portname, delay=50):
     """Send contents of sysex file to given MIDI output.
     Reads file given by filename and sends all consecutive sysex messages found
     in it to given midiout after prompt.
     """
+
+    SYSTEM_EXCLUSIVE = b'\xF0'
+    END_OF_EXCLUSIVE = b'\xF7'
     bn = basename(filename)
 
     with open(filename, 'rb') as sysex_file:
         data = sysex_file.read()
 
         if data.startswith(SYSTEM_EXCLUSIVE):
-            try:
-                if prompt:
-                    yn = raw_input("Send '%s' to %s (y/N)? " % (bn, portname))
-            except (EOFError, KeyboardInterrupt):
-                print('')
-                raise StopIteration
+            sox = 0
+            i = 0
 
-            if not prompt or yn.lower() in ('y', 'yes'):
-                sox = 0
-                i = 0
+            while sox >= 0:
+                sox = data.find(SYSTEM_EXCLUSIVE, sox)
 
-                while sox >= 0:
-                    sox = data.find(SYSTEM_EXCLUSIVE, sox)
+                if sox >= 0:
+                    eox = data.find(END_OF_EXCLUSIVE, sox)
 
-                    if sox >= 0:
-                        eox = data.find(END_OF_EXCLUSIVE, sox)
+                    if eox >= 0:
+                        sysex_msg = data[sox:eox + 1]
+                        # Python 2: convert data into list of integers
+                        if isinstance(sysex_msg, str):
+                            sysex_msg = [ord(c) for c in sysex_msg]
 
-                        if eox >= 0:
-                            sysex_msg = data[sox:eox + 1]
-                            # Python 2: convert data into list of integers
-                            if isinstance(sysex_msg, str):
-                                sysex_msg = [ord(c) for c in sysex_msg]
+                        midiout.send_message(sysex_msg)
+                        time.sleep(0.001 * delay)
 
-                            log.info("Sending '%s' message #%03i...", bn, i)
-                            midiout.send_message(sysex_msg)
-                            time.sleep(0.001 * delay)
+                        i += 1
+                    else:
+                        break
 
-                            i += 1
-                        else:
-                            break
-
-                        sox = eox + 1
-        else:
-            log.warning("File '%s' does not start with a sysex message.", bn)
+                    sox = eox + 1
 
 
 def enter_bootloader():
     portname = "Duo MIDI 1"
-    midiout, portname = open_midioutput(portname, interactive=False, use_virtual=True)
-    print("Sending sysex")
-    send_sysex_file("./DUO-bootloader.syx", midiout, "Duo MIDI 1", prompt=False)
-    print("done")
+    midiout, portname = open_midioutput(
+        portname, interactive=False, use_virtual=True)
+
+    send_sysex_file("./DUO-bootloader.syx", midiout, "Duo MIDI 1")
+
+
+def find_sdp_interface():
+    match sdp_usb.scan_usb("0x1FC9,0x0145"):
+        case [interface]:
+            return interface
+        case _:
+            print("No DUO connected")
+
+
+def find_mboot_interface():
+    match mboot_usb.scan_usb("0x15A2,0x0073"):
+        case [interface]:
+            return interface
+        case _:
+            print("No DUO connected")
+
+
+def update_firmware(firmware_path):
+    with open(firmware_path, "rb") as firmware:
+        firmware_bytes = firmware.read()
+
+    try:
+        enter_bootloader()
+    except rtmidi._rtmidi.InvalidPortError:
+        pass
+
+    time.sleep(1)
+    interface = find_sdp_interface()
+
+    print("Sending flashloader")
+    with SDP(interface) as s:
+        flashloader_bytes = open("./ivt_flashloader.bin", "rb").read()
+        flashloader_addr = 0x20205800
+        s.write_file(flashloader_addr, flashloader_bytes)
+        s.jump_and_run(flashloader_addr)
+
+    print("Sent flashloader. Rebooting.")
+    time.sleep(1)
+
+    boot_interface = find_mboot_interface()
+
+    with McuBoot(boot_interface) as mboot:
+        mboot.get_property(1, 0)
+        mboot.fill_memory(0x20202000, 4, 0xC0000007)
+        mboot.fill_memory(0x20202004, 4, 0)
+        "Configuring memory"
+        mboot.configure_memory(0x20202000, 9)
+
+        binary_start_addr = 0x60000400
+
+        print("Erasing flash")
+        print(len(firmware_bytes))
+        mboot.flash_erase_region(binary_start_addr, 128000)
+        print("Done")
+
+        print("Writing binary")
+        mboot.write_memory(binary_start_addr, firmware_bytes)
+        print("Done")
+
+        print("Resetting")
+        mboot.reset(reopen=False)
 
 
 def main():
-    enter_bootloader()
+    firmware_path = "./duo_firmware.bin"
+    if len(sys.argv) > 1:
+        firmware_path = sys.argv[1]
+
+    update_firmware(firmware_path)
 
 
 if __name__ == "__main__":
