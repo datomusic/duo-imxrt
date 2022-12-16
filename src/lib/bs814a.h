2611278ad995d2e195009ffa909f9dcba70d9117
Sundström Valter <mail@valter.nu>
2022-12-16 12:07:01 +0100

    References

--- src/lib/bs814a.h
+++ src/lib/bs814a.h
@@ -8,11 +8,11 @@ const auto KEY_COUNT = 4;
 
 typedef uint8_t TouchState;
 
-inline bool down(TouchState state, uint8_t index){
+inline bool down(TouchState &state, uint8_t index){
   return (state & (1 << index));
 };
 
-inline bool pressed(TouchState state, TouchState previous, uint8_t index){
+inline bool pressed(TouchState &state, TouchState &previous, uint8_t index){
   const int mask = (1 << index);
   return (state & mask) && !(previous & mask) ;
 };
