1ce23a85108ade23aec22e23fa8edf8f4003c7a7
David Menting <david@davidmenting.nl>
2022-12-21 14:25:20 +0100

    Remove SYNC LED benchmark pin initialization

--- src/lib/sync.cpp
+++ src/lib/sync.cpp
@@ -12,12 +12,7 @@ namespace Sync {
         IOMUXC_SetPinMux(SYNC_OUT_PINMUX, 0U);
         gpio_pin_config_t sync_out_config = { kGPIO_DigitalOutput, 0};
         GPIO_PinInit(SYNC_OUT_PORT, SYNC_OUT_PIN, &sync_out_config);
-        
-        IOMUXC_SetPinMux(LED2_PINMUX, 0U);
-        gpio_pin_config_t led2_config = { kGPIO_DigitalOutput, 0};
-        GPIO_PinInit(LED2_PORT, LED2_PIN, &led2_config);
     }
-    
 
     uint32_t read() {
         return GPIO_PinRead(SYNC_IN_PORT, SYNC_IN_PIN);
