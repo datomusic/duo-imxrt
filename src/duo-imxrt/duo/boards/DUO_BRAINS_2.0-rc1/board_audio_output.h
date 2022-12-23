5a3820f0ff7121c8333f84e165484d276a506db5
David Menting <david@davidmenting.nl>
2022-12-23 19:38:29 +0100

    Make headphone output volume more reasonable

--- src/duo-imxrt/duo/boards/DUO_BRAINS_2.0-rc1/board_audio_output.h
+++ src/duo-imxrt/duo/boards/DUO_BRAINS_2.0-rc1/board_audio_output.h
@@ -5,12 +5,12 @@
 
 typedef AudioOutputPT8211 BoardAudioOutput;
 
-const float MAIN_GAIN      = 0.8f;
+const float MAIN_GAIN      = 0.5f;
 const float DELAY_GAIN     = 0.6f;
 const float KICK_GAIN      = 0.8f;
 const float HAT_GAIN       = 0.9f;
 
-const float HEADPHONE_GAIN = 1.42f;
-const float SPEAKER_GAIN   = 3.6f;
+const float HEADPHONE_GAIN = 2.1f;
+const float SPEAKER_GAIN   = 2.5f;
 
 #endif /* end of include guard: AUDIO_OUTPUT_H_VS0HQTKT */
