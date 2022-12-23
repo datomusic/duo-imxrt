332da76d13fb87059c1f0b2baca6192235cf5a19
David Menting <david@davidmenting.nl>
2022-12-23 14:51:59 +0100

    Decrease volume of the headphone output

--- src/duo-imxrt/duo/boards/DUO_BRAINS_2.0-rc1/board_audio_output.h
+++ src/duo-imxrt/duo/boards/DUO_BRAINS_2.0-rc1/board_audio_output.h
@@ -10,7 +10,7 @@ const float DELAY_GAIN     = 0.6f;
 const float KICK_GAIN      = 0.8f;
 const float HAT_GAIN       = 0.9f;
 
-const float HEADPHONE_GAIN = 4.2f;
+const float HEADPHONE_GAIN = 1.42f;
 const float SPEAKER_GAIN   = 3.6f;
 
 #endif /* end of include guard: AUDIO_OUTPUT_H_VS0HQTKT */
