d786016b5a7e5475eefa8b6039e79a1c2663e85f
David Menting <david@davidmenting.nl>
2022-12-23 13:33:28 +0100

    Brought USB identifiers in line with K20 DUO firmware

--- src/lib/usb/usb_descriptors.c
+++ src/lib/usb/usb_descriptors.c
@@ -93,8 +93,8 @@ uint8_t const *tud_descriptor_configuration_cb(uint8_t index) {
 // array of pointer to string descriptors
 char const *string_desc_arr[3] = {
     (const char[]){0x09, 0x04}, // 0: is supported language is English (0x0409)
-    "DATO",                     // 1: Manufacturer
-    "Duo"                       // 2: Product
+    "Dato",                     // 1: Manufacturer
+    "Dato DUO"                  // 2: Product
 };
 
 static uint16_t _desc_str[32];
