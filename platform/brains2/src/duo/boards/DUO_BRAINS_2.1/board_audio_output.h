e2144950bde1c3bd4969c195060827666d25a89e
David Menting <david@davidmenting.nl>
2023-05-24 11:49:44 +0200

    Better audio levels based on measurements

--- platform/brains2/src/duo/boards/DUO_BRAINS_2.1/board_audio_output.h
+++ platform/brains2/src/duo/boards/DUO_BRAINS_2.1/board_audio_output.h
@@ -5,18 +5,11 @@
 
 typedef AudioOutputPT8211 BoardAudioOutput;
 
-/* const float MAIN_GAIN      = 0.5f; */
-const float MAIN_GAIN      = 1.25f;
-/* const float DELAY_GAIN     = 0.6f; */
-const float DELAY_GAIN     = 1.5f;
-/* const float KICK_GAIN      = 0.8f; */
+const float MAIN_GAIN      = 0.8f;
+const float DELAY_GAIN     = 1.0f;
 const float KICK_GAIN      = 2.0f;
-/* const float HAT_GAIN       = 0.9f; */
-const float HAT_GAIN       = 2.25f;
+const float HAT_GAIN       = 2.0;
 
-/* const float HEADPHONE_GAIN = 2.1f; */
-/* const float SPEAKER_GAIN   = 2.5f; */
-
-const float HEADPHONE_GAIN = 0.84f;
+const float HEADPHONE_GAIN = 1.0f;
 const float SPEAKER_GAIN   = 1.0f;
 #endif /* end of include guard: AUDIO_OUTPUT_H_VS0HQTKT */
