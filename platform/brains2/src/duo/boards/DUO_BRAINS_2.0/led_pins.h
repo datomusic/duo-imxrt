509c1354e814649b4674d76c29c58ffbad3ece88
Sundström Valter <mail@valter.nu>
2023-06-26 12:23:25 +0200

    Fixup

--- platform/brains2/src/duo/boards/DUO_BRAINS_2.0/led_pins.h
+++ platform/brains2/src/duo/boards/DUO_BRAINS_2.0/led_pins.h
@@ -5,11 +5,13 @@
 #define PIN_LED_2 GPIO_05
 #define PIN_LED_3 GPIO_01
 
-#define write_osc_led(value) analogWrite(PIN_LED_1, 128 - ((value) >> 3));
+#define write_env_led(value)                                                   \
+  analogWrite(PIN_LED_3, 254 - (uint16_t)(value * 254.0f));
+
+#define write_osc_led(value)                                                   \
+  analogWrite(PIN_LED_1, (uint16_t)(value / 4.03f) + 1);
 
 #define write_filter_led(value)                                                \
-  analogWrite(PIN_LED_2, 1 + ((value * value) >> 13));
-
-#define write_env_led(value) analogWrite(PIN_LED_3, (value * 254.0f) + 1);
+  analogWrite(PIN_LED_2, 254 - (uint16_t)((value * value) / 4128));
 
 #endif /* end of include guard: LED_PINS_H_ZSKLTGQH */
