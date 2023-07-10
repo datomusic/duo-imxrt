b34dcb6c896d9d03518464f3de0ca8dc1020090a
David <david@davidmenting.nl>
2023-07-10 12:22:13 +0200

    Make accent do more (#119)
    * Reduce kick drum clipping
    
    * Don't initalize update_time to remove flicker
    
    * Set resonance to 4.0 while accent is pressed
    
    * Protect against possible overflow in tempo.cpp
    
    * Fix tempo.h include
    
    ---------
    
    Co-authored-by: Sundström Valter <mail@valter.nu>

--- platform/brains2/src/duo/boards/DUO_BRAINS_2.1/board_audio_output.h
+++ platform/brains2/src/duo/boards/DUO_BRAINS_2.1/board_audio_output.h
@@ -7,8 +7,8 @@ typedef AudioOutputPT8211 BoardAudioOutput;
 
 const float MAIN_GAIN      = 0.8f;
 const float DELAY_GAIN     = 1.0f;
-const float KICK_GAIN      = 2.0f;
-const float HAT_GAIN       = 2.0;
+const float KICK_GAIN      = 1.0f;
+const float HAT_GAIN       = 1.2f;
 
 const float HEADPHONE_GAIN = 1.0f;
 const float SPEAKER_GAIN   = 1.0f;
