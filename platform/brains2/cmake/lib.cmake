85795d2c09b9ca4eed30c52772dc8ad8bc872a22
David Menting <david@davidmenting.nl>
2023-09-18 11:53:44 +0200

    Include flexio in modules and not in lib

--- platform/brains2/cmake/lib.cmake
+++ platform/brains2/cmake/lib.cmake
@@ -14,7 +14,6 @@ file(GLOB LIB_SRC_FILES
 
 target_sources(${EXECUTABLE_NAME} PRIVATE
   ${LIB_SRC_FILES}
-  ${CMAKE_CURRENT_LIST_DIR}/../devices/MIMXRT1011/drivers/fsl_flexio.c
   ${LIB_SOURCE_DIR}/custom_teensy_audio/AudioStream.cpp
   ${LIB_SOURCE_DIR}/custom_teensy_audio/output_mqs.cpp
   ${LIB_SOURCE_DIR}/custom_teensy_audio/output_pt8211.cpp
