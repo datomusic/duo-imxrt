450f94848e411a2a398000c58d4235b87e0ef250
David Menting <david@davidmenting.nl>
2023-11-21 14:53:30 +0100

    Volume scaling for Brains 2.0

--- platform/brains2/src/duo/boards/DUO_BRAINS_2.0/board_audio_output.h
+++ platform/brains2/src/duo/boards/DUO_BRAINS_2.0/board_audio_output.h
@@ -5,12 +5,22 @@
 
 typedef AudioOutputPT8211 BoardAudioOutput;
 
-const float MAIN_GAIN      = 0.5f;
-const float DELAY_GAIN     = 0.6f;
-const float KICK_GAIN      = 0.8f;
-const float HAT_GAIN       = 0.9f;
-
 const float HEADPHONE_GAIN = 2.1f;
 const float SPEAKER_GAIN   = 2.5f;
+
+const float HEADPHONE_MAIN_GAIN = 0.5f;
+const float HEADPHONE_DELAY_GAIN = 0.6f;
+const float HEADPHONE_KICK_GAIN = 0.8f;
+const float HEADPHONE_HAT_GAIN = 0.9f;
+
+const float SPEAKER_MAIN_GAIN = 0.5f;
+const float SPEAKER_DELAY_GAIN = 0.6f;
+const float SPEAKER_KICK_GAIN = 0.8f;
+const float SPEAKER_HAT_GAIN = 0.9f;
+
+float MAIN_GAIN      = 0.5f;
+float DELAY_GAIN     = 0.6f;
+float KICK_GAIN      = 0.8f;
+float HAT_GAIN       = 0.9f;
 
 #endif /* end of include guard: AUDIO_OUTPUT_H_VS0HQTKT */
