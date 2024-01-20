2aca373d347baa2a9f5cd369b160277da8f0742c
Sundström Valter <mail@valter.nu>
2024-01-20 02:41:50 +0100

    Helper macro for calling both serial and usb

--- platform/brains2/src/lib/midi.cpp
+++ platform/brains2/src/lib/midi.cpp
@@ -8,74 +8,65 @@ static MIDI_NAMESPACE::SerialMIDI<HardwareSerial> serialTransport(Serial);
 static MIDI_NAMESPACE::MidiInterface<MIDI_NAMESPACE::SerialMIDI<HardwareSerial>>
     serial(serialTransport);
 
+#define BOTH(function_call)                                                    \
+  serial.function_call;                                                        \
+  usb.function_call;
+
 void MIDI_IO::init(const byte channel) {
-  serial.begin(channel);
-  usb.begin(channel);
+  BOTH(begin());
 }
 
 void MIDI_IO::handle(const byte channel) {
-  serial.read(channel);
-  usb.read(channel);
+  BOTH(read(channel));
 }
 
 void MIDI_IO::setHandleNoteOn(NoteCallback callback) {
-  serial.setHandleNoteOn(callback);
-  usb.setHandleNoteOn(callback);
+  BOTH(setHandleNoteOn(callback));
 }
 void MIDI_IO::setHandleNoteOff(NoteCallback callback) {
-  serial.setHandleNoteOff(callback);
-  usb.setHandleNoteOff(callback);
+  BOTH(setHandleNoteOff(callback));
 }
 
 void MIDI_IO::setHandleControlChange(NoteCallback callback) {
-  serial.setHandleControlChange(callback);
-  usb.setHandleControlChange(callback);
+  BOTH(setHandleControlChange(callback));
 }
 
 void MIDI_IO::setHandleStart(VoidCallback callback) {
-  serial.setHandleStart(callback);
-  usb.setHandleStart(callback);
+  BOTH(setHandleStart(callback));
 }
 
 void MIDI_IO::setHandleStop(VoidCallback callback) {
-  serial.setHandleStop(callback);
-  usb.setHandleStop(callback);
+  BOTH(setHandleStop(callback));
 }
 
 void MIDI_IO::setHandleContinue(VoidCallback callback) {
-  serial.setHandleContinue(callback);
-  usb.setHandleContinue(callback);
+  BOTH(setHandleContinue(callback));
 }
 
 void MIDI_IO::setHandleClock(VoidCallback callback) {
-  usb.setHandleClock(callback);
+  BOTH(setHandleClock(callback));
 }
 
 void MIDI_IO::setHandleSystemExclusive(SyxCallback callback) {
-  usb.setHandleSystemExclusive(callback);
+  BOTH(setHandleSystemExclusive(callback));
 }
 
 void MIDI_IO::sendRealtime(const midi::MidiType message) {
-  serial.sendRealTime(message);
-  usb.sendRealTime(message);
+  BOTH(sendRealTime(message));
 }
 
 void MIDI_IO::sendControlChange(byte cc, byte value, byte channel) {
-  serial.sendControlChange(cc, value, channel);
-  usb.sendControlChange(cc, value, channel);
+  BOTH(sendControlChange(cc, value, channel));
 }
 
 void MIDI_IO::sendNoteOn(byte inNoteNumber, byte inVelocity, byte inChannel) {
-  serial.sendNoteOn(inNoteNumber, inVelocity, inChannel);
-  usb.sendNoteOn(inNoteNumber, inVelocity, inChannel);
+  BOTH(sendNoteOn(inNoteNumber, inVelocity, inChannel));
 }
 
 void MIDI_IO::sendNoteOff(byte inNoteNumber, byte inVelocity, byte inChannel) {
-  serial.sendNoteOff(inNoteNumber, inVelocity, inChannel);
-  usb.sendNoteOff(inNoteNumber, inVelocity, inChannel);
+  BOTH(sendNoteOff(inNoteNumber, inVelocity, inChannel));
 }
 
 void MIDI_IO::sendSysEx(unsigned length, const byte *bytes) {
-  serial.sendSysEx(length, bytes);
-  usb.sendSysEx(length, bytes);
+  BOTH(sendSysEx(length, bytes));
 }
