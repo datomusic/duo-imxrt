0b69346a90c595b4ffa706679b8a17dfc8027f63
Sundström Valter <mail@valter.nu>
2024-01-27 16:04:24 +0100

    Move submodules into deps

--- brains2/cmake/usb.cmake
+++ brains2/cmake/usb.cmake
@@ -1,30 +1,17 @@
 
-# TinyUSB defines
-add_definitions(
-  -DCFG_TUSB_MCU=OPT_MCU_MIMXRT11XX
-  -DCFG_TUD_MIDI_RX_BUFSIZE=512
-  -DCFG_TUD_CDC_RX_BUFSIZE=512
-  -DCFG_TUD_MIDI_TX_BUFSIZE=512
-  -DCFG_TUD_CDC_TX_BUFSIZE=512
-  -DCFG_TUD_MSC_BUFSIZE=1024
-)
-
 target_sources(${EXECUTABLE_NAME} PRIVATE
   ${LIB_SOURCE_DIR}/usb/usb.cpp
   ${LIB_SOURCE_DIR}/usb/usb_descriptors.c
   ${LIB_SOURCE_DIR}/usb/midi_usb_bridge/MIDIUSB.cpp
 )
 
-set(TINYUSB_SRC ${CMAKE_CURRENT_LIST_DIR}/../../modules/tinyusb/src)
-set(ARDUINO_MIDI_LIBRARY_SRC ${CMAKE_CURRENT_LIST_DIR}/../../modules/arduino_midi_library/src)
-
 target_include_directories(${EXECUTABLE_NAME} PRIVATE
-  ${TINYUSB_SRC}
   ${LIB_SOURCE_DIR}/usb
   ${LIB_SOURCE_DIR}/usb/midi_usb_bridge
-  ${ARDUINO_MIDI_LIBRARY_SRC}
-  ${CMAKE_CURRENT_LIST_DIR}/../../modules/Arduino-USBMIDI/src
+  ${DEPS_DIR}/arduino_midi_library/src
+  ${DEPS_DIR}/Arduino-USBMIDI/src
+  ${DEPS_DIR}/tinyusb/src
 )
 
-include(${DEPS_DIR}/component_tinyusb.cmake)
+include(${DEPS_DIR}/tinyusb.cmake)
 include(${DEPS_DIR}/arduino_midi_library/src/component_arduino_midi_library.cmake)
--- brains2/deps/tinyusb.cmake
+++ brains2/deps/tinyusb.cmake
@@ -1,7 +1,18 @@
 include_guard(GLOBAL)
 message("TinyUSB component is included.")
 
-set(TINYUSB_SRC ${CMAKE_CURRENT_LIST_DIR}/../../modules/tinyusb/src)
+# TinyUSB defines
+add_definitions(
+  -DCFG_TUSB_MCU=OPT_MCU_MIMXRT11XX
+  -DCFG_TUD_MIDI_RX_BUFSIZE=512
+  -DCFG_TUD_CDC_RX_BUFSIZE=512
+  -DCFG_TUD_MIDI_TX_BUFSIZE=512
+  -DCFG_TUD_CDC_TX_BUFSIZE=512
+  -DCFG_TUD_MSC_BUFSIZE=1024
+)
+
+
+set(TINYUSB_SRC ${CMAKE_CURRENT_LIST_DIR}/tinyusb/src)
 
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
     ${TINYUSB_SRC}/class/cdc/cdc_device.c
