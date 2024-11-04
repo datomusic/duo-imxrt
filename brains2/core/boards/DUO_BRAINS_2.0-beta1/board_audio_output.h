054653f8d0b7c58476f9bc60213b8369745fa002
Sundström Valter <mail@valter.nu>
2024-11-04 18:24:17 +0100

    Add missing configuration board audio configuration for 2.0-beta1

--- brains2/core/boards/DUO_BRAINS_2.0-beta1/board_audio_output.h
+++ brains2/core/boards/DUO_BRAINS_2.0-beta1/board_audio_output.h
@@ -5,10 +5,20 @@
 
 typedef AudioOutputMQS BoardAudioOutput;
 
-const float MAIN_GAIN      = 0.5f;
-const float DELAY_GAIN     = 0.6f;
-const float KICK_GAIN      = 0.8f;
-const float HAT_GAIN       = 0.9f;
+float MAIN_GAIN      = 0.5f;
+float DELAY_GAIN     = 0.6f;
+float KICK_GAIN      = 0.8f;
+float HAT_GAIN       = 0.9f;
+
+const float HEADPHONE_MAIN_GAIN = 0.5f;
+const float HEADPHONE_DELAY_GAIN = 0.6f;
+const float HEADPHONE_KICK_GAIN = 0.8f;
+const float HEADPHONE_HAT_GAIN = 0.9f;
+
+const float SPEAKER_MAIN_GAIN = 0.5f;
+const float SPEAKER_DELAY_GAIN = 0.6f;
+const float SPEAKER_KICK_GAIN = 0.8f;
+const float SPEAKER_HAT_GAIN = 0.9f;
 
 const float HEADPHONE_GAIN = 2.1f;
 const float SPEAKER_GAIN   = 2.5f;
