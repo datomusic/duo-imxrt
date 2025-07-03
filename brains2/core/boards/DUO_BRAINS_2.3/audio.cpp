ed8a5eaf14e0383ec377668a058037303556ee1b
David <david@davidmenting.nl>
2025-07-03 14:33:26 +0200

    [WIP] Brains 2.3 support (#182)
    * style: Reformat indentation in audio.h
    
    * feat: add amp_init function to initialize and store amp enable polarity
    
    * Indentation and Audio::amp_init()
    
    * Add amp_init
    
    * Add Brains 2.3 with amp polarity detect
    
    * Add aider files to gitignore

--- brains2/core/boards/DUO_BRAINS_2.3/audio.cpp
+++ brains2/core/boards/DUO_BRAINS_2.3/audio.cpp
@@ -9,6 +9,8 @@
 #define AMP_MUTE_PIN 24U
 
 namespace Audio {
+bool amp_enable_polarity = false;
+
 void headphone_enable(void) {
   IOMUXC_SetPinMux(HP_ENABLE_PINMUX, 0U);
   gpio_pin_config_t hp_enable_config = {kGPIO_DigitalOutput, 0};
@@ -23,24 +25,33 @@ void headphone_disable(void) {
   GPIO_PinWrite(HP_ENABLE_PORT, HP_ENABLE_PIN, 0);
 }
 
-void amp_init(void) {
-
-}
-
 void amp_enable(void) {
-  IOMUXC_SetPinMux(AMP_MUTE_PINMUX, 0U);
-  gpio_pin_config_t amp_mute_config = {kGPIO_DigitalOutput, 0};
-  GPIO_PinInit(AMP_MUTE_PORT, AMP_MUTE_PIN, &amp_mute_config);
-  GPIO_PinWrite(AMP_MUTE_PORT, AMP_MUTE_PIN, 0);
+  GPIO_PinWrite(AMP_MUTE_PORT, AMP_MUTE_PIN, (amp_enable_polarity ? 1 : 0));
 }
 
 void amp_disable(void) {
+  GPIO_PinWrite(AMP_MUTE_PORT, AMP_MUTE_PIN, (amp_enable_polarity ? 0 : 1));
+}
+
+void amp_init(void) {
+  // BIT 12 should be 0 to disable pull/keeper
+  const uint32_t IOMUXC_PINCONFIG_DEFAULT = 0xB0U;
+
   IOMUXC_SetPinMux(AMP_MUTE_PINMUX, 0U);
-  gpio_pin_config_t amp_mute_config = {kGPIO_DigitalOutput, 0};
+  IOMUXC_SetPinConfig(AMP_MUTE_PINMUX, IOMUXC_PINCONFIG_DEFAULT);
+  gpio_pin_config_t amp_mute_config = {kGPIO_DigitalInput, 0};
   GPIO_PinInit(AMP_MUTE_PORT, AMP_MUTE_PIN, &amp_mute_config);
-  GPIO_PinWrite(AMP_MUTE_PORT, AMP_MUTE_PIN, 1);
-  amp_mute_config = {kGPIO_DigitalInput, 0};
+  
+  // An external resistor sets the amp to mute at startup. 
+  // Some chips are active high others are active low
+  // Read its direction to determine the polarity
+  amp_enable_polarity = !(GPIO_PinRead(AMP_MUTE_PORT, AMP_MUTE_PIN));
+  
+  IOMUXC_SetPinConfig(AMP_MUTE_PINMUX, (1<<12) | IOMUXC_PINCONFIG_DEFAULT);
+  // Reconfigure as output with initial state
+  amp_mute_config.direction = kGPIO_DigitalOutput;
   GPIO_PinInit(AMP_MUTE_PORT, AMP_MUTE_PIN, &amp_mute_config);
+  GPIO_PinWrite(AMP_MUTE_PORT, AMP_MUTE_PIN, (amp_enable_polarity ? 0 : 1));
 }
 
 } // namespace Audio
--- brains2/core/lib/audio.h
+++ brains2/core/lib/audio.h
@@ -2,10 +2,12 @@
 #define AUDIO_H_OVLTWHKY
 
 namespace Audio {
-void headphone_enable(void);
-void headphone_disable(void);
-void amp_enable(void);
-void amp_disable(void);
+  void headphone_enable(void);
+  void headphone_disable(void);
+  void amp_init(void);
+  void amp_enable(void);
+  void amp_disable(void);
+  extern bool amp_enable_polarity;
 } // namespace Audio
 
 #endif /* end of include guard: AUDIO_H_OVLTWHKY */
